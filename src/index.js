9d38fae5d7b1565e9f267159a04236bc706daba6
DESKTOP-4R5VPBI\DONGCT <hdongqt@gmail.com>
2022-12-04 19:34:25 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -40,6 +40,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(process.env.PORT || 3300, () => {
-  console.log("listening on port 3300");
+app.get("/", (req, res) => {
+  res.sendStatus(200);
 });
